d6ce0f99b077331bd517152fdb4a1cf6cb5134c3
Michael Legemah <michaellegemah@gmail.com>
2023-09-23 14:29:40 -0400

    added bad chain test

--- backend/tests/blockchain/test_blockchain.py
+++ backend/tests/blockchain/test_blockchain.py
@@ -42,3 +42,16 @@ def test_replace_chain(blockchain_three_blocks):
     blockchain.replace_chain(blockchain_three_blocks.chain)
 
     assert blockchain.chain == blockchain_three_blocks.chain
+
+def test_replace_chain_not_longer():
+    blockchain = Blockchain()
+    
+    with pytest.raises(Exception, match='The incoming chain must be longer'):
+        blockchain_three_blocks.replace_chain(blockchain.chain)
+        
+def test_replace_chain_bad_chain(blockchain_three_blocks):
+    blockchain = Blockchain()
+    blockchain_three_blocks.chain[1].hash = 'evil hash'
+    
+    with pytest.raises(Exception, match='The incoming chain is invalid')
+        blockchain.replace_chain(blockchain_three_blocks.chain)
