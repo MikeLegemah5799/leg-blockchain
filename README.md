5a106ff6dcd447b38c43f8e1fc9046369b6c8790
Michael Legemah <michaellegemah@gmail.com>
2023-09-23 13:59:57 -0400

    installed tk

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ source blockchain-env/bin/activate
 ```
 pip3 install -r requirements.txt
 ```
+
+**Create virtual environment**
+```
+python3 -m venv blockchain-env
+```
+
+**Run Virtual environment**
+```
+source blockchain-env/bin/activate
+```
--- backend/tests/blockchain/test_blockchain.py
+++ backend/tests/blockchain/test_blockchain.py
@@ -3,6 +3,7 @@ import pytest
 from backend.blockchain.blockchain import Blockchain
 from backend.blockchain.block import GENESIS_DATA
 
+
 def test_blockchain_instance():
     blockchain = Blockchain()
     assert blockchain.chain[0].hash == GENESIS_DATA['hash']
@@ -14,10 +15,23 @@ def test_add_block():
     blockchain.add_block(data)
 
     assert blockchain.chain[-1].data == data
-    
-def test_is_valid_chain():
+
+
+@pytest.fixture
+def blockchain_three_blocks():
     blockchain = Blockchain()
     for i in range(3):
         blockchain.add_block(i)
-        
-    Blockchain.is_valid_chain(blockchain.chain)
+    return blockchain
+
+
+def test_is_valid_chain(blockchain_three_blocks):
+
+    Blockchain.is_valid_chain(blockchain_three_blocks.chain)
+
+
+def test_is_valid_chain_bad_genesis(blockchain_three_blocks):
+    blockchain_three_blocks.chain[0].hash = 'evil_hash'
+
+    with pytest.raises(Exception, match='genesis block must be valid'):
+        Blockchain.is_valid_chain(blockchain_three_blocks)
