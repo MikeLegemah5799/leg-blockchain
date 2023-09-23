a75777873155341b8d914f809b51a577de64eca5
Michael Legemah <michaellegemah@gmail.com>
2023-09-23 19:36:26 -0400

    added in keys

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -1,2 +1,7 @@
-subscribe_key = 'sub-c-be4a94c1-bbcf-4252-b35c-6a614803564d'
-publish_key = 'pub-c-1bfd86fa-1d8e-448c-a2fe-d445c3a84954'
+from pubnub.pubnub import PubNub
+from pubnub.pnconfiguration import PNConfiguration
+
+pnconfig = PNConfiguration()
+pnconfig.subscribe_key = 'sub-c-be4a94c1-bbcf-4252-b35c-6a614803564d'
+pnconfig.publish_key = 'pub-c-1bfd86fa-1d8e-448c-a2fe-d445c3a84954'
+pubnub = PubNub(pnconfig)
