f3ada4c02cdc5dba1a690ac17bcc381e6c06cdee
Michael Legemah <michaellegemah@gmail.com>
2023-09-23 17:18:41 -0400

    added in flask

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/test')
-def test():
-    return 'Test'
+@app.route('/')
+def default():
+    return 'Welcome to My Blockchain'
 
 
 app.run()
