b50edca7107dc0f90e2bb5be74c0b9bb44ed94f8
Michael Legemah <michaellegemah@gmail.com>
2023-09-23 14:14:51 -0400

    replace chain

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -1,4 +1,3 @@
-from turtle import st
 from backend.blockchain.block import Block
 
 
@@ -17,7 +16,25 @@ class Blockchain:
 
     def __repr__(self):
         return f'Blockchain: {self.chain}'
-    
+
+    def replace_chain(self, chain):
+        """
+        Replace the local chain with the incoming one if the following applies:
+            -The incoming chain is longer than the local one.
+            -The incoming chain is formatted properly.
+        """
+        if len(chain) <= len(self.chain):
+            raise Exception(
+                'Cannot replace. The incoming chain must be longer.')
+
+        try:
+            Blockchain.is_valid_chain(chain)
+        except Exception as e:
+            raise Exception(
+                f'Cannot replace. The incoming chain is invalid: {e}')
+
+        self.chain = chain
+
     @staticmethod
     def is_valid_chain(chain):
         """
@@ -26,15 +43,15 @@ class Blockchain:
         - the chain must start with the genesis block
         - blocks must be formatted correctly
         """
-        
+
         if chain[0] != Block.genesis():
             raise Exception('The genesis block must be valid')
-        
-        
+
         for i in range(1, len(chain)):
             block = chain[i]
             last_block = chain[i-1]
             Block.is_valid_block(last_block, block)
+
 
 def main():
     blockchain = Blockchain()
